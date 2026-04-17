88645e6be141046b4f493436e53bb2e5bfc84174
EDISON RAMOS <eramoz@MacBook-Pro-de-EDISON.local>
2026-04-17 06:01:38 -0500

    titulo

--- src/App.tsx
+++ src/App.tsx
@@ -45,6 +45,9 @@ export default function App() {
 
   return (
     <div className="app-container">
+      <div className="app-title">
+        <h1>Sistema de Gestión de la Calidad</h1>
+      </div>
       <div className="map-section">
         <ReactFlow
           nodes={nodes}
